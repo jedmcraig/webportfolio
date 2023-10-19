4e29cae9676b1963dec6204693ece433cfbe1500
jmacaraig <johnedison.macaraig@gmail.com>
2023-10-19 17:10:47 +0800

    final capstone

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js"
+// import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js"
 
 const primaryNav = document.querySelector('.primary-navigation')
 const navBorder = document.querySelector('.nav-border')
 const navToggle = document.querySelector('.nav-custom')
-const scrollTracker = document.querySelector('.scroller-tracker')
+// const scrollTracker = document.querySelector('.scroller-tracker')
 const textBoxAnimate = document.querySelectorAll('.text-container')
 const progress = document.querySelector('.progress')
+const workText = document.querySelector('.worktext')
 let show = document.querySelectorAll('.show')
 
 
@@ -43,22 +44,22 @@ const typingEffect = new Typed(".dynamic-text", {
     backDelay: 1500,
 })
 
-const scrollTrackingTimeline = new ScrollTimeline({
-    source: document.scrollingElement,
-    orientation: "block",
-    scrollOffsets: [CSS.percent(20), CSS.percent(100)],
-})
+// const scrollTrackingTimeline = new ScrollTimeline({
+//     source: document.scrollingElement,
+//     orientation: "block",
+//     scrollOffsets: [CSS.percent(20), CSS.percent(100)],
+// })
 
 
-scrollTracker.animate(
-    {
-        transform: ["scaleX(0)", "scaleX(1)"]
-    },
-    {
-        duration: 1,
-        timeline: scrollTrackingTimeline,
-    }
-)
+// scrollTracker.animate(
+//     {
+//         transform: ["scaleX(0)", "scaleX(1)"]
+//     },
+//     {
+//         duration: 1,
+//         timeline: scrollTrackingTimeline,
+//     }
+// )
 
 // textBoxAnimate.forEach((textBox) => {
     
@@ -92,11 +93,11 @@ scrollTracker.animate(
 function textBoxes () {
 
     // const triggerBottom = window.innerHeight / 2; // 50% from the bottom of the screen
-    const trigger300px = window.innerHeight - 300;
+    const trigger200px = window.innerHeight - 200;
 
     textBoxAnimate.forEach(box => {
         const boxTop = box.getBoundingClientRect().top;
-        const isBoxVisible = boxTop < trigger300px;
+        const isBoxVisible = boxTop < trigger200px;
         let progressHeight;
 
         if (isBoxVisible && !box.classList.contains('show')) {
@@ -105,8 +106,6 @@ function textBoxes () {
 
             progressHeight = ((show.length-1) / 5) * 100;
             progress.style.height = progressHeight + '%';
-            console.log(show.length)
-            console.log(progress.style.height);
 
         } else if (!isBoxVisible && box.classList.contains('show')) {
             box.classList.remove('show');
@@ -120,3 +119,64 @@ function textBoxes () {
 }
 
 window.addEventListener('scroll', textBoxes)
+
+
+
+// Form Validation
+const form = document.querySelector(".contact-form");
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+const messages = document.querySelectorAll(".message");
+
+const error = (input, message) => {
+  input.nextElementSibling.classList.add("error");
+  input.nextElementSibling.textContent = message;
+};
+
+const success = (input) => {
+  input.nextElementSibling.classList.remove("error");
+};
+
+const checkRequiredFields = (inputArr) => {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === "") {
+      error(input, `${input.id} is required`);
+    }
+  });
+};
+
+const checkLength = (input, min) => {
+  if (input.value.trim().length < min) {
+    error(input, `${input.id} must be at least ${min} characters`);
+  } else {
+    success(input);
+  }
+};
+
+const checkEmail = (input) => {
+  const regEx =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (regEx.test(input.value.trim())) {
+    success(input);
+  } else {
+    error(input, "Email is not valid");
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  checkLength(username, 2);
+  checkLength(subject, 2);
+  checkLength(message, 10);
+  checkEmail(email);
+  checkRequiredFields([username, email, subject, message]);
+
+  const notValid = Array.from(messages).find((message) => {
+    return message.classList.contains("error");
+  });
+
+  notValid && e.preventDefault();
+});
+// End of Form Validation
